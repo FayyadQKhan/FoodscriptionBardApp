13828eb9832006457e78ee1aa8d985898fbd8442
FayyadQKhan <140316336+FayyadQKhan@users.noreply.github.com>
2023-07-28 18:32:39 +0530

    Update foodscription_bard_app.py

--- foodscription_bard_app.py
+++ foodscription_bard_app.py
@@ -1,14 +1,78 @@
+from bardapi import Bard
+import os
 import streamlit as st
-import bard
+from streamlit_chat import message
+import json
+# import re
 
-def get_bard_response(query):
-  response = bard.query(query)
-  return response
 
-st.title("Bard in Streamlit")
+os.environ["_BARD_API_KEY"] = "YwhglmGAiBMVRJuUFhOCDEfVRVTG2_5EQcGfjYU42EvoyAlu-Jq_nL-ODfQauMRzN56pXA."
 
-query = st.text_input("Enter a query:")
+# message = input("Ask: ")
+# print(Bard().get_answer(str(message))['content'])
+# cache = memory.Memory()
 
-response = get_bard_response(query)
+st.title("Foodscription - with Google Bard")
 
-st.write(response)
+def response_api(prompt):
+    message = Bard().get_answer(str(prompt))['content']
+    # cache[prompt] = message
+    return message
+
+def user_input():
+    input_text = st.text_input("Paste Ingredients: ")
+    return input_text
+
+# def extract_substring(string):
+#   """Extracts a substring from a given string that starts with '{' and ends with '}'.
+
+#   Args:
+#     string: The string to extract the substring from.
+
+#   Returns:
+#     The extracted substring.
+#   """
+
+#   pattern = r"{*}"
+#   match = re.search(pattern, string)
+
+#   if match is None:
+#     return None
+
+#   return match
+
+# if 'generate' not in st.session_state:
+#     st.session_state['generate'] = []
+
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
+
+ingredient = user_input()
+# ingredient = input('Enter ingredient: ')
+engineered_prompt = "You are a json only output machine give me ingredient and its corresponding foodgroup in the format {ingredient:foodgroup} where given ingredient is key and foodgroup is value: " + ingredient
+
+if ingredient:
+    output = response_api(engineered_prompt)
+    start = output.find('{')
+    end = output.find('}')
+    json_ = output[start:end+1]
+    json_ = json.loads(json_)
+    # output = str(output)
+    # for line in output:
+    #     json_expr = "{"+line.partition("{")[2]
+    #     output = json.loads(json_expr)
+
+    st.write(json_)
+    # st.session_state.generate.append(output)
+    # st.session_state.past.append(ingredient)
+
+    # print(type(output))
+
+
+# print(type(json_))
+# print(json_)
+
+# if st.session_state['generate']:
+#     for i in range(len(st.session_state['generate'])-1,-1,-1):
+#         message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+#         message(st.session_state["generate"][i], key = str(i))
